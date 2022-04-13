e5eb4f1b8ada1b8c83cda20d3e90d99e14c83c9e
Denes <jose.araujo@zup.com.br>
2022-04-13 11:18:10 -0300

    metodo deletar no controlador de autor implmentado

--- src/main/java/br/com/zup/edu/autores/controller/AutorController.java
+++ src/main/java/br/com/zup/edu/autores/controller/AutorController.java
@@ -4,11 +4,15 @@ import java.net.URI;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.zup.edu.autores.model.Autor;
@@ -28,8 +32,22 @@ private final AutorRepository repository;
 	ResponseEntity<Void> cadastrar(@RequestBody @Valid AutorDTO request, UriComponentsBuilder uriComponentsBuilder){
 		Autor novoAutor = request.paraAutor();
 		
+		repository.save(novoAutor);
+		
 		URI location = uriComponentsBuilder.path("/autores/{id}").buildAndExpand(novoAutor.getId()).toUri();
 		
 		return ResponseEntity.created(location).build();
 	}
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remove(@PathVariable("id") Long idAutor){
+		
+		Autor autor = repository.findById(idAutor).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não existe cadastro de album para o id informado"));
+		
+		repository.delete(autor);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
 }
